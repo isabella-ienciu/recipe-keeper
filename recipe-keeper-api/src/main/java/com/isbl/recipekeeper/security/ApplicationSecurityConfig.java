89b07fe25204d31611a49c92a52d1a993dad97d4
Isabella <isabellaienciu@gmail.com>
2021-08-13 16:56:07 +0300

    add password encryption with bcrypt

--- recipe-keeper-api/src/main/java/com/isbl/recipekeeper/security/ApplicationSecurityConfig.java
+++ recipe-keeper-api/src/main/java/com/isbl/recipekeeper/security/ApplicationSecurityConfig.java
@@ -1,13 +1,26 @@
 package com.isbl.recipekeeper.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder){
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -18,5 +31,16 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService(){
+        UserDetails isabellaUser = User.builder()
+                .username("isabella")
+                .password(passwordEncoder.encode("password"))
+                .roles("ADMIN") //ROLE_ADMIN
+                .build();
+        return new InMemoryUserDetailsManager(isabellaUser);
     }
 }
